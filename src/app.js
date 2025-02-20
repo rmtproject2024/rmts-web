5664136abc1d4cd0589f614f3ecb01057744bbe1
Hamza-Khalifa <Hamza.khalifa.876@gmail.com>
2025-02-21 00:53:18 +0300

    update

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = require("./routes");
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
+
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const SERVER_HOST = process.env.SERVER_HOST || '0.0.0.0';
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5173'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true  
+}));
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(router);
+
+
+app.options('*', cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.listen(SERVER_PORT, SERVER_HOST, () => {
+    console.log(`Listening on ${SERVER_HOST}:${SERVER_PORT}`);
+});
+
+module.exports = app;
---
+++ src/config/firebase.js
@@ -0,0 +1,42 @@
+const firebase = require("firebase/app");
+const admin = require('firebase-admin');
+const { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  sendEmailVerification, 
+  sendPasswordResetEmail
+} = require("firebase/auth");
+
+// Use require to load the service account JSON file
+const serviceAccount = require("C:\\Users\\GT\\Desktop\\Firebase-authentication-Express.js-main\\Firebase-authentication-Express.js-main\\firebaseservice.json"); // Update with the correct path to your firebaseservice.json file
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// Firebase client config
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Xkl5IVT7Z-nFuFaHjd1H0RILvyHITVQ",
+  authDomain: "rmts-8f76b.firebaseapp.com",
+  projectId: "rmts-8f76b",
+  storageBucket: "rmts-8f76b.appspot.com",
+  messagingSenderId: "890480272228",
+  appId: "1:890480272228:web:e78e6adc4132343a30d071",
+  measurementId: "G-5X3BK2CFFE",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+module.exports = {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  admin
+};
